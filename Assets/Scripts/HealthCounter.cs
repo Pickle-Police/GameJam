8c8735f4b2736f5dea105adc32c14d7aeef66f50
Pickle-Police <95372128+Pickle-Police@users.noreply.github.com>
2022-01-18 10:04:41 -0800

    Created Health Counter

--- Assets/Scripts/HealthCounter.cs
+++ Assets/Scripts/HealthCounter.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthCounter : MonoBehaviour
 {
-    //private var lifeCount : private object guiGO;
+    public float lifeCount = 20f;
+    float currentLifeCount;
+    public Text lifeCountText;
 
-    //public object LifeCount { get => lifeCount; set => lifeCount = value; }
+    void Start()
+    {
+        currentLifeCount = lifeCount;
+        lifeCountText.text = "Health: " + currentLifeCount.ToString();
+    }
 
-    //int = 4;
-    //public guiGO : GUIText;
-    //function DecrementLife ()
-    //{
-    //    --lifeCount;
-    //    guiGO.text = lifeCount;
-    //}
+    public void TakeDamage(float amt)
+    {
+        currentLifeCount -= amt;
+        lifeCountText.text = "Health: " + currentLifeCount.ToString();
+
+        if (currentLifeCount <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
 }
