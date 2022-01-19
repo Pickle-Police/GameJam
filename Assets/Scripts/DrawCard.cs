a53d407d5e3fa46e165311b7c60a263ba7ed6bbf
dannyboy126 <scheibmeirdaniel@gmail.com>
2022-01-19 10:16:29 -0800

    changed the code

--- Assets/Scripts/DrawCard.cs
+++ Assets/Scripts/DrawCard.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class DrawCard : MonoBehaviour
 {
-    public GameObject deck1;
+    public List<GameObject> cards = new List<GameObject>();
+
     public GameObject PlayerArea;
     public GameObject EnemyArea;
 
+    public GameObject DevitoCard;
+
     void Start()
     {
-      
+        cards.Add(DevitoCard);
     }
     
     public void OnClick()
     {
         for (var i = 0; i < 5; i++)
         {
-         GameObject playerCard = Instantiate(deck1, new Vector3(0, 0, 0), Quaternion.identity);
+         GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
          playerCard.transform.SetParent(PlayerArea.transform, false);
         }
         
     }
 
-    
+  
 }
 
  
